d5a2c74422ed84fc01f1b2124550a9709912a627
reynaldyAl <reynaldy.al123@gmail.com>
2026-09-23 07:34:39 +0800

    chore: Refactor Tailwind configuration and update tutorial styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,30 +3,15 @@ module.exports = {
     content: ["./**/*.{html,js}"],
     theme: {
         extend: {
-
+          colors: {
+            midnight: '#121063'
+          },
+          fontFamily: {
+            glory: ['"Give You Glory"', 'cursive']
+          },
         },
     },
     plugins: [],
 }
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // Memindai seluruh file HTML & JS di proyek
-  content: ["./**/*.{html,js}"],
 
-  theme: {
-    // "extend" berfungsi MENAMBAHKAN tanpa menghapus warna/font bawaan Tailwind
-    extend: {
-      colors: {
-        midnight: "#121063",
-        brand: "#06b6d4",
-      },
-      fontFamily: {
-        heading: ["Poppins", "sans-serif"],
-        glory: ["Give You Glory", "cursive"],
-      },
-    },
-  },
-
-  plugins: [],
-}
